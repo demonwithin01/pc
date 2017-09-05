171faa2dd14de0360136187c05be392e474b2ed4
Stephen <stephenbarnes88@gmail.com>
2017-09-05 12:59:57 +1000

    Updated the content folder layout.

--- PCArchitect/App_Start/BundleConfig.cs
+++ PCArchitect/App_Start/BundleConfig.cs
@@ -8,24 +8,50 @@ namespace PCArchitect
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles( BundleCollection bundles )
         {
-            bundles.Add( new ScriptBundle( "~/bundles/jquery" ).Include(
-                        "~/Scripts/jquery-{version}.js" ) );
+            bundles.Add( new ScriptBundle( "~/bundles/external" ).Include(
+                         /* JQuery */
+                         EScript( "jquery-3.1.1" ),
+                         EScript( "jquery.validate" ),
+                         EScript( "jquery.validate.unobtrusive" ),
 
-            bundles.Add( new ScriptBundle( "~/bundles/jqueryval" ).Include(
-                        "~/Scripts/jquery.validate*" ) );
+                         EScript( "bootstrap" ) ) );
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add( new ScriptBundle( "~/bundles/modernizr" ).Include(
-                        "~/Scripts/modernizr-*" ) );
+            //bundles.Add( new ScriptBundle( "~/bundles/pc-architect" ).Include(
+            //             PcaScript( "" ) ) );
 
-            bundles.Add( new ScriptBundle( "~/bundles/bootstrap" ).Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js" ) );
+            bundles.Add( new StyleBundle( "~/bundles/css" ).Include(
+                         CSS( "bootstrap" ),
+                         CSS( "site" ) ) );
+        }
 
-            bundles.Add( new StyleBundle( "~/Content/css" ).Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css" ) );
+        /// <summary>
+        /// Generates the full location of the external script to be included.
+        /// </summary>
+        /// <param name="scriptName">The name of the script to include, do not include the file type.</param>
+        /// <returns>The virtual location of the external script.</returns>
+        private static string EScript( string scriptName )
+        {
+            return "~/content/scripts/external/" + scriptName + ".js";
+        }
+
+        /// <summary>
+        /// Generates the full location of the PC Architect script to be included.
+        /// </summary>
+        /// <param name="scriptName">The name of the script to include, do not include the file type.</param>
+        /// <returns>The virtual location of the PC Architect script.</returns>
+        private static string PcaScript( string scriptName )
+        {
+            return "~/content/scripts/pca/" + scriptName + ".js";
+        }
+
+        /// <summary>
+        /// Generates the full location of the CSS to be included.
+        /// </summary>
+        /// <param name="fileName">The name of the style to include, do not include the file type.</param>
+        /// <returns>The virtual location of the CSS file.</returns>
+        private static string CSS( string fileName )
+        {
+            return "~/content/css/" + fileName + ".css";
         }
     }
 }
---
+++ PCArchitect/Content/scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="external/jquery-3.1.1.js" />
+/// <reference path="external/jquery.validate.js" />
+/// <reference path="external/jquery.validate.unobtrusive.js" />
+/// <reference path="external/bootstrap.js" />
